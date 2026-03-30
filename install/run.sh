6e282a957ea217e4b7594ebcccf2af7b397f26db
sanghwa <sanghwa318@naver.com>
2026-03-30 11:26:48 +0900

    update install dir 2nd

--- install/run.sh
+++ install/run.sh
@@ -12,21 +12,23 @@ load_env_file "${VARS_DIR}/cluster.env"
 load_env_file "${VARS_DIR}/network.env"
 load_env_file "${VARS_DIR}/registry.env"
 load_env_file "${VARS_DIR}/bastion.env"
+load_env_file "${VARS_DIR}/install-config.env"
 load_env_file "${VARS_DIR}/post.env"
 
-MODE="${1:-all}"
+MODE="${1:-}"
 
 run_pre() {
   print_section "RUN PRE"
-  bash "${SCRIPT_DIR}/01-pre/00-make-certs.sh"
-  bash "${SCRIPT_DIR}/01-pre/04-command-extract.sh"
-  bash "${SCRIPT_DIR}/01-pre/01-registry.sh"
-  bash "${SCRIPT_DIR}/01-pre/05-bastion-chrony.sh"
-  bash "${SCRIPT_DIR}/01-pre/03-bastion-account.sh"
-  bash "${SCRIPT_DIR}/01-pre/06-disable-selinux.sh"
-  bash "${SCRIPT_DIR}/01-pre/07-hosts-render.sh"
-  bash "${SCRIPT_DIR}/01-pre/08-dns-render.sh"
-  bash "${SCRIPT_DIR}/01-pre/09-haproxy-render.sh"
+  bash "${SCRIPT_DIR}/01-pre/00-command-extract.sh"
+  bash "${SCRIPT_DIR}/01-pre/01-disable-selinux.sh"
+  bash "${SCRIPT_DIR}/01-pre/02-bastion-account.sh"
+  bash "${SCRIPT_DIR}/01-pre/03-bastion-chrony.sh"
+  bash "${SCRIPT_DIR}/01-pre/04-make-certs.sh"
+  bash "${SCRIPT_DIR}/01-pre/05-registry.sh"
+  bash "${SCRIPT_DIR}/01-pre/06-hosts-render.sh"
+  bash "${SCRIPT_DIR}/01-pre/07-dns-render.sh"
+  bash "${SCRIPT_DIR}/01-pre/08-haproxy-render.sh"
+  bash "${SCRIPT_DIR}/01-pre/09-tftp-install.sh"
   bash "${SCRIPT_DIR}/01-pre/10-dhcp-render.sh"
   bash "${SCRIPT_DIR}/01-pre/11-pxe-grub-render.sh"
   bash "${SCRIPT_DIR}/01-pre/12-keepalived-render.sh"
@@ -43,24 +45,19 @@ run_install() {
 
 run_post() {
   print_section "RUN POST"
-  bash "${SCRIPT_DIR}/01-pre/02-openshift-admin-user.sh"
-  bash "${SCRIPT_DIR}/03-post/06-ingress-master.sh"
-  bash "${SCRIPT_DIR}/03-post/07-whereabouts-reconciler.sh"
-  bash "${SCRIPT_DIR}/03-post/08-userWorkloadMonitoring.sh"
-  bash "${SCRIPT_DIR}/03-post/09-routingViaHost.sh"
-  bash "${SCRIPT_DIR}/03-post/10-enableCatalogSources.sh"
+  bash "${SCRIPT_DIR}/03-post/00-openshift-admin-user.sh"
+  bash "${SCRIPT_DIR}/03-post/01-ingress-master.sh"
+  bash "${SCRIPT_DIR}/03-post/02-whereabouts-reconciler.sh"
+  bash "${SCRIPT_DIR}/03-post/03-userWorkloadMonitoring.sh"
+  bash "${SCRIPT_DIR}/03-post/04-routingViaHost.sh"
+  bash "${SCRIPT_DIR}/03-post/05-enableCatalogSources.sh"
 }
 
 case "${MODE}" in
   pre) run_pre ;;
   install) run_install ;;
   post) run_post ;;
-  all)
-    run_pre
-    run_install
-    run_post
-    ;;
   *)
-    die "usage: $0 [pre|install|post|all]"
+    die "usage: $0 [pre|install|post]"
     ;;
 esac
